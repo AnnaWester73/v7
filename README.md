9e44f6f0124b589699bc586ee16d2849803230df
Anna Wester <annawester73@gmail.com>
2026-05-03 21:04:44 +0200

    commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# E2E-test med Playwright
+
+Det här projektet innehåller end-to-end-tester för webbsidan:
+
+https://lejonmanen.github.io/agile-helper/
+
+Tester är skrivna i Python med Playwright och Pytest.
+
+## Installation
+
+Installera beroenden:
+
+```bash
+pip install pytest-playwright
+playwright install
---
+++ v7_1/test/e2e/test_agile_helper.py
@@ -0,0 +1,26 @@
+import re
+from playwright.sync_api import Page, expect
+
+URL = "https://lejonmanen.github.io/agile-helper/"
+
+# Test User story 1
+
+def test_user_story_1_sprint_planning(page: Page):
+    page.goto(URL)
+
+    page.get_by_test_id("btn-first").click()
+    page.get_by_role("button", name="Börja sprinten med Sprint").click()
+    page.get_by_role("heading", name="Sprint planning").click()
+
+    expect(page.get_by_role("heading", name="Sprint planning")).to_be_visible()
+
+# Test User story 2
+
+def test_user_story_2_byta_sprak(page: Page):
+    page.goto(URL)
+
+    page.get_by_test_id("language-en").click()
+    expect(page.get_by_role("button", name="First")).to_be_visible()
+
+    page.get_by_test_id("language-sv").click()
+    expect(page.get_by_role("button", name="Första")).to_be_visible()
